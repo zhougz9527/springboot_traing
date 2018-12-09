8a1b2a248160575b16adf2fa300d23cfbf8652f0
jinmu <1649563336@qq.com>
2018-12-09 19:21:57 +0800

    first commit

---
+++ src/main/java/com/example/springboot_traing/SpringbootTraingApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_traing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTraingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTraingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springboot_traing/SpringbootTraingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot_traing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTraingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
